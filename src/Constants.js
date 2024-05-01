2d382de7e122d25c3f146364778e73b882c2d463
Deep13 <deep.deepak30@gmail.com>
2024-05-02 00:30:23 +0530

    commit

---
+++ src/Constants.js
@@ -0,0 +1,199 @@
+export const day_title = 'Hijri Mubarak';
+export const banner_key1 = 'ca-app-pub-4717579333914549/4681921126';
+export const banner_key2 = 'ca-app-pub-4717579333914549/3112854432';
+export const banner_key3 = 'ca-app-pub-4717579333914549/4490349433';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/9392495785';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/1672614402';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/4107206057';
+export const language = ['English', 'Hindi', 'Arabic', 'Urdu'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'May Allah bless you with his choicest blessings. Happy Hijri to you and your family.',
+      },
+      {
+        id: '002',
+        text: 'A very blissful Happy Hijri to you and your family.',
+      },
+      {
+        id: '003',
+        text:
+          'May your life be always filled with light, love, happiness and good health. Happy Hijri.',
+      },
+      {
+        id: '004',
+        text:
+          'Hijri is all about sharing what we have and caring for those in need. May you have a wonderful Happy Hijri!',
+      },
+      {
+        id: '005',
+        text: 'Happy Hijri to you and everyone in your family.',
+      },
+      {
+        id: '006',
+        text:
+          'May Allah bless you with all that you have wished for. Happy Hijri',
+      },
+      {
+        id: '007',
+        text:
+          'Wishing you a very Happy Happy Hijri. Cherish every moment of this beautiful festival and may all your dreams come true soon.',
+      },
+      {
+        id: '008',
+        text:
+          'This Hijri, may Allah bless you with good health and happiness. Happy Hijri',
+      },
+      {
+        id: '009',
+        text:
+          'May Allah give you all the prosperity and success. May Allah bless you with wealth and happiness and give you a healthy life. Happy Hijri.',
+      },
+      {
+        id: '010',
+        text:
+          "Here's wishing a blessed Hijri that will inspire you with courage and strength to help you to win every challenge of life! Happy Hijri.",
+      },
+    ]
+  }
+  ,
+  Hindi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'अल्लाह आपको अपनी पसंद का आशीर्वाद दे। आपको और आपके परिवार को हिजरी मुबारक।',
+      },
+      {
+        id: '002',
+        text: 'आपको और आपके परिवार को हिजरी की बहुत बहुत मुबारकबाद।',
+      },
+      {
+        id: '003',
+        text:
+          'आपका जीवन हमेशा प्रकाश, प्रेम, खुशी और अच्छे स्वास्थ्य से भरा रहे। हैप्पी हिजरी।',
+      },
+      {
+        id: '004',
+        text:
+          'हिजरी हमारे पास जो कुछ है उसे साझा करने और जरूरतमंद लोगों की देखभाल करने के बारे में है। आपके पास एक अद्भुत हैप्पी हिजरी हो!',
+      },
+      {
+        id: '005',
+        text: 'आपको और आपके परिवार में सभी को हिजरी मुबारक।।',
+      },
+      {
+        id: '006',
+        text: 'अल्लाह आपको वो सब दे जो आपने चाहा है। हैप्पी हिजरी',
+      },
+      {
+        id: '007',
+        text:
+          'आपको हिजरी की ढेर सारी शुभकामनाएं। इस खूबसूरत त्योहार के हर पल को संजोएं और आपके सभी सपने जल्द ही सच हों।',
+      },
+      {
+        id: '008',
+        text: 'यह हिजरी, अल्लाह आपको अच्छी सेहत और खुशियाँ दे। हैप्पी हिजरी',
+      },
+      {
+        id: '009',
+        text:
+          'अल्लाह आप सभी को समृद्धि और सफलता दे। ईश्वर आपको धन और खुशियाँ प्रदान करें और आपको स्वस्थ जीवन प्रदान करें। हैप्पी हिजरी।',
+      },
+    ]
+  },
+  Arabic: {
+    "wishes": [
+      {
+        id: '001',
+        text: 'بارك الله فيك باختيار بركاته. هجري سعيد لك ولعائلتك.',
+      },
+      {
+        id: '002',
+        text: 'هجرية سعيدة جدا لك ولعائلتك.',
+      },
+      {
+        id: '003',
+        text:
+          'أتمنى أن تكون حياتك مليئة بالنور والحب والسعادة والصحة الجيدة. هجري سعيد.',
+      },
+      {
+        id: '004',
+        text:
+          'الهجري هو كل شيء عن مشاركة ما لدينا والاهتمام بالمحتاجين. أتمنى لك هجرية سعيدة رائعة!',
+      },
+      {
+        id: '005',
+        text: 'هجري سعيد لك ولكل فرد في عائلتك.',
+      },
+      {
+        id: '006',
+        text: 'بارك الله فيك بكل ما تمنيت. هجري سعيد',
+      },
+      {
+        id: '007',
+        text:
+          'أتمنى لكم هجرياً سعيداً جداً. نعتز بكل لحظة في هذا المهرجان الجميل وقد تتحقق كل أحلامك قريبًا.',
+      },
+      {
+        id: '008',
+        text: 'هذا الهجري بارك الله فيك بالصحة والسعادة. هجري سعيد',
+      },
+      {
+        id: '009',
+        text:
+          'وفقك الله كل التوفيق والنجاح. بارك الله فيك بالمال والسعادة ، ويعطيك حياة صحية. هجري سعيد.',
+      },
+    ]
+  },
+  Urdu: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'اللہ پاک آپ کو اپنی بہترین برکتوں سے نوازے۔ آپ اور آپ کے اہل خانہ کو ہجری مبارک ہو۔',
+      },
+      {
+        id: '002',
+        text: 'آپ اور آپ کے اہل خانہ کو بہت خوش کن مبارک ہجری',
+      },
+      {
+        id: '003',
+        text:
+          'آپ کی زندگی ہمیشہ روشنی ، محبت ، خوشی اور اچھی صحت سے معمور رہے۔ مبارک ہجری',
+      },
+      {
+        id: '004',
+        text:
+          'ہجری ہمارے پاس جو کچھ ہے اسے بانٹنا اور ضرورت مندوں کی دیکھ بھال کرنا ہے۔ آپ کو ایک حیرت انگیز ہجری مبارک ہو!',
+      },
+      {
+        id: '005',
+        text: 'ہجری آپ کو اور آپ کے اہل خانہ میں سب کو مبارک ہو۔',
+      },
+      {
+        id: '006',
+        text: 'اللہ آپ سب کی توفیق دے جس کی تمنا کرتے ہو۔ مبارک ہجری',
+      },
+      {
+        id: '007',
+        text:
+          'آپ کو بہت ہی مبارک ہو مبارک ہجری اس خوبصورت تہوار کے ہر لمحے کو پسند کریں اور آپ کے تمام خواب جلد ہی پورے ہوجائیں۔',
+      },
+      {
+        id: '008',
+        text: 'یہ ہجری ، اللہ آپ کو اچھی صحت اور خوشی سے نوازے۔ مبارک ہجری',
+      },
+      {
+        id: '009',
+        text:
+          'اللہ آپ سب کو خوشحالی اور کامیابی عطا فرمائے۔ اللہ آپ کو دولت اور خوشی سے نوازے اور صحت مند زندگی عطا کرے۔ مبارک ہجری',
+      },
+    ]
+  }
+
+
+};
